0ac893a52fd653e20e611e177f7a150fa337db5e
Fusion528 <73862913+Fusion528@users.noreply.github.com>
2021-02-26 21:36:01 -0600

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+local PI = game.PlaceId
+
+if PI == 5784984978 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fusion528/SimulatorHub/main/MilkSim", true))()
+elseif PI == 2423809996 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fusion528/SimulatorHub/main/IceCreamSim", true))()
+elseif PI == 4520536611 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fusion528/SimulatorHub/main/FitnessSimulator.lua",true))()
+end
